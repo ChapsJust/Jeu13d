dc80e18e427b3cc9d913f593e05766b684ca7f91
chapsjust <2238424@carrefour.cegepvicto.ca>
2024-11-06 15:46:57 -0500

    AjoutAnimMarche Zombie + Fix

--- Assets/cree/Scripts/EnemyController.cs
+++ Assets/cree/Scripts/EnemyController.cs
@@ -5,10 +5,12 @@ public class EnemyController : MonoBehaviour
     // Code inspiré pour l'ennemi:  https://www.youtube.com/watch?v=UvDqnbjEEak
     private Transform player;
     private NavMeshAgent enemy;
+    private Animator animator;
 
     private void Awake()
     {
         enemy = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
 
         GameObject playerObjet = GameObject.FindGameObjectWithTag("Player");
         if(playerObjet != null)
@@ -17,7 +19,11 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
-        if(player != null) 
+        if(player != null)
+        {
             enemy.SetDestination(player.position);
+            bool isWalking = enemy.velocity.magnitude > 0.1f;
+            animator.SetBool("IsWalking", isWalking);
+        }
     }
 }
